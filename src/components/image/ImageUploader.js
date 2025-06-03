fd37df6b07eb912a0328b81b1f6677c305d82032
NguyenSiVanHao <HaoNSVSE172181@fpt.edu.vn>
2025-06-03 23:57:38 +0700

    [MB-0004] Register UI

---
+++ src/components/image/ImageUploader.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Image,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+
+export default function ImageUploader() {
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    // Yêu cầu quyền truy cập
+    const permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!permissionResult.granted) {
+      alert("Bạn cần cho phép truy cập thư viện ảnh!");
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        Hãy gửi hình ảnh thẻ sinh viên để xác minh (nếu có)
+      </Text>
+      <Button title="Chọn ảnh từ thư viện" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={styles.image} />}
+      <TouchableOpacity
+        onPress={() => console.log("Gửi form")}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Đăng ký</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    marginBottom: 20,
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 16,
+    marginVertical: 10,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    marginVertical: 10,
+    borderRadius: 8,
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: "#007BFF",
+    padding: 15,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
---
+++ src/screens/register/Register.js
@@ -0,0 +1,71 @@
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from "react-native";
+import React from "react";
+import ImageUploader from "../../components/image/ImageUploader";
+
+const Register = () => {
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <TextInput style={styles.textInput} placeholder="Tên" />
+        <TextInput style={styles.textInput} placeholder="Họ" />
+        <TextInput style={styles.textInput} placeholder="Tên Đăng Nhập" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Mật Khẩu"
+          secureTextEntry
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Xác Nhận Mật Khẩu"
+          secureTextEntry
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Số Điện Thoại"
+          keyboardType="numeric"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Email"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <ImageUploader />
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+  },
+  textInput: {
+    height: 50,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 16,
+    borderColor: "#ddd",
+    fontSize: 16,
+    backgroundColor: "#fff",
+  },
+});
+
+export default Register;
